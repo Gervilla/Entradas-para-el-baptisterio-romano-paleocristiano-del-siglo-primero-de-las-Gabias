d3b4cb4b8d0cdeb58007de1ebd67c190fe1030d5
Carrillo Fernandez <mcarril2@t-systems.es>
2022-11-14 17:32:14 +0100

    modificar errores2

---
+++ src/main/java/com/curso/entradasbaptisterioromanopaleocristiano/Sintickets.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.curso.entradasbaptisterioromanopaleocristiano;
+
+/**
+ *
+ * @author mcarril2
+ */
+public class Sintickets extends Exception{
+    public Sintickets(String mensaje){
+        super(mensaje);
+    }
+}
